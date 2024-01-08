177476d4303cb1a2db2b680e30b2e6ad67507e7d
Annu Kamat <annukamat@adobe.com>
2024-01-08 22:39:10 +0530

    update

--- LinkedList/l001basic.java
+++ LinkedList/l001basic.java
@@ -1,8 +1,6 @@
-/**
- * l001basic
- */
 import java.io.*;
 import java.util.*;
+
 public class l001basic {
     public static class Node {
         int data;
@@ -83,6 +81,124 @@ public class l001basic {
                 return temp.data;
             }
         }
+
+        public void addFirst(int val){
+            Node temp = new Node();
+            temp.data = val;
+
+            if(size == 0){
+                head = tail = temp;
+            }else{
+                temp.next = head;
+                head = temp;
+            }
+            size++;
+        }
+
+        public void addAt(int idx, int val){
+            if(idx<0 || idx>size) 
+                System.out.println("Invalid Arguments");
+            else if(idx==0) addFirst(val);
+            else if(idx == size) addLast(val);
+            else{
+                Node node = new Node();
+                node.data = val;
+
+                Node temp = head;
+
+                for(int i=0; i<idx-1; i++){
+                    temp = temp.next;
+                }
+                node.next = temp.next;
+                temp.next = node;
+
+                size++;
+            }
+
+            
+
+
+        }
+
+        public void removeLast(){
+            if(size==0){
+                System.out.println("List is empty");
+            }else if(size == 1){
+                head = tail=null;
+                size = 0;
+            }else{
+                Node temp = head;
+            
+                for(int i=0; i<size-2; i++){
+                    temp = temp.next;
+                }
+                tail = temp;
+                temp.next = null;
+
+                size--;
+            }
+        }
+
+        public void removeAt(int idx){
+            if(idx<0 || idx>=size){
+                System.out.println("Invalid Arguments");
+            }else if(idx==0) removeFirst();
+            else if(idx == size-1) removeLast();
+            else{
+                Node temp = head;
+                for(int i=0; i<idx-1; idx++){
+                    temp = temp.next;
+                }
+                Node forw = temp.next;
+                temp.next = forw.next;
+                forw.next = null;
+
+                size--;
+            }
+        }
+
+        // reverse a ll data iterative
+        public Node getNodeAt(int idx){
+            Node temp = head;
+            for(int i=0; i<idx; i++){
+                temp = temp.next;
+            }
+            return temp;
+        }
+
+        public void reverseDI(){
+            int li = 0;
+            int ri = size-1;
+
+            while(li<ri){
+                Node left = getNodeAt(li);
+                Node right = getNodeAt(ri);
+
+                int temp = left.data;
+                left.data = right.data;
+                right.data = temp;
+
+                li++;
+                ri--;
+            }
+        }
+
+        public void reversePI(){
+            Node cur = head;
+            Node prev = null;
+
+            while(cur != null){
+                Node forw = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = forw;
+            }
+            // swap head and tail
+            Node temp = head;
+            head = tail;
+            tail = temp;
+        }
+
 
     }
 
